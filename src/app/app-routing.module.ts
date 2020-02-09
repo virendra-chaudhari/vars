fb9137f652201aadcdb790b2f21572769879fd4a
virendra <virenchaudhari07@gmail.com>
2020-02-09 09:54:43 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CourcesComponent } from './cources/cources.component';
+import { HomeComponent } from './home/home.component';
+import { PlacestudentComponent } from './placestudent/placestudent.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "about", component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'cources', component: CourcesComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'placestudent', component: PlacestudentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
