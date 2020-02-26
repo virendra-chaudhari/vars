f0f197df651c800c32913b7802f74586bafde0f1
virendra <virenchaudhari07@gmail.com>
2020-02-26 23:27:07 +0530

    ui changes

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -18,4 +18,17 @@ export class FooterComponent {
   ngOnDestroy() {
 
   }
+  websites:any[]=[
+    {id:1,web:"Twitter",imgPath:"fa fa-twitter", url:'https://www.twitter.com'},
+    {id:2,web:"facebook",imgPath:"fa fa-facebook", url:'https://www.facebook.com/vars.technosoft'},
+    {id:3,web:"google+",imgPath:"fa fa-google-plus" ,url:'https://www.google.com'},
+    {id:3,web:"instagram",imgPath:"fa fa-instagram",url:'https://www.instagram.com'},
+    {id:4,web:"gmail",imgPath:"fa fa-pinterest",url:'https://www.pinterest.com'},
+    {id:4,web:"linkedin",imgPath:"fa fa-linkedin",url:'https://www.linkedin.com'},
+    {id:4,web:"linkedin",imgPath:"fa fa-youtube",url:'https://youtube.com/'}
+    
+  ]
+  onSelect(website){
+    window.open(website);
+ }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,32 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
+  placementLogo: any[] = [
+    {
+      companyName: "NCPL"
+      , companyLogo: "logoNcpl.png"
+    },
+    {
+      companyName: "OrnageBits"
+      , companyLogo: "orangeBitsLogo.png"
+    },
+    {
+      companyName: "Pixel6"
+      , companyLogo: "pixel6Logo.png"
+    },{
+      companyName: "Selgrm"
+      , companyLogo: "selgumLogo.jpg"
+    },
+    {
+      companyName: "selgem"
+      , companyLogo: "selgemLogo.png"
+    },
+    {
+      companyName: "Proser Digital"
+      , companyLogo: "proserLogo.jpg"
+    }
+  ]
 
 }
