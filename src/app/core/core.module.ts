d5ea62af24bd71d1d128a1c4c6fb817f640f6aab
virendra <virenchaudhari07@gmail.com>
2020-02-27 14:08:20 +0530

    get changes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BrowserModule } from '@angular/platform-browser'
 
 @NgModule({
   imports: [
+    BrowserModule
   ],
   exports: [
     HeaderComponent,
